2e9280037cb2e4ca1bc8574dcda5ad9a8985c2bc
tassosvanis@outlook.com.gr <tassosvanis@outlook.com.gr>
2020-07-04 13:30:58 +0300

    qr_code_script

---
+++ my_qrcode_script.py
@@ -0,0 +1,23 @@
+import qrcode
+import cv2
+import matplotlib.pyplot as plt
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+data= 'https://google.com'
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('./qrcode_test.png')
+
+# print(img)
+
+im = cv2.imread('qrcode_test.png')
+im_resized = cv2.resize(im, (224, 224), interpolation=cv2.INTER_LINEAR)
+#
+plt.imshow(cv2.cvtColor(im_resized, cv2.COLOR_BGR2RGB))
+plt.show()
