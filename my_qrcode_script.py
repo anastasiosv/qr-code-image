7901f659d31b58852d6e247bfd739b7c8b0a4fec
tassosvanis@outlook.com.gr <tassosvanis@outlook.com.gr>
2020-07-04 14:15:05 +0300

    some changes

--- my_qrcode_script.py
+++ my_qrcode_script.py
@@ -8,16 +8,16 @@ qr = qrcode.QRCode(
     box_size=10,
     border=4,
 )
-data= 'https://google.com'
+
+data= 'https://github.com/tassosv/qr-code-image'
 qr.add_data(data)
 qr.make(fit=True)
+
 img = qr.make_image(fill_color="black", back_color="white")
-img.save('./qrcode_test.png')
+img.save('git_page.png')
 
-# print(img)
-
-im = cv2.imread('qrcode_test.png')
+im = cv2.imread('git_page.png')
 im_resized = cv2.resize(im, (224, 224), interpolation=cv2.INTER_LINEAR)
-#
+
 plt.imshow(cv2.cvtColor(im_resized, cv2.COLOR_BGR2RGB))
 plt.show()
